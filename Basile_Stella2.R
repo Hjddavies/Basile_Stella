5d9519632b76b99ece44fa91beee39fc0d787104
Hywel Davies <hywel.davies.2@city.ac.uk>
2019-04-04 19:12:24 +0100

    fixing error with Stackoverflow.com

---
+++ Basile_Stella2.R
@@ -0,0 +1,21 @@
+library(stringr)
+library(twitteR)
+library(purrr)
+library(tidytext)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(scales)
+library(broom)
+library(ggplot2)
+library(tidyverse)
+
+consumerKey = "QqstUdQ2yLRUGboueH7wJIsv1"
+consumerSecret = "ykRtbBKSarjpQYRBCUBUHaiuW4FnPz2iDLO2TkcEtO9mocYOTN"
+accessToken = "240583200-1R98JE0NLVS1CzwngMk5zOywJzd4Ia2MMYX7jMF3"
+accessSecret = "SdVrYx1Gdu0lNPBxAw6AkkQBiTcLUHPc6SrPNNPqttKGd"
+options(httr_oauth_cache=TRUE)
+
+#fix found on https://stackoverflow.com/questions/29634342/unable-to-use-the-setup-twitter-oauth-function-in-r-to-work-with-twitter
+twitteR:::setup_twitter_oauth(consumer_key = consumerKey, consumer_secret = consumerSecret,
+                    access_token = accessToken, access_secret = accessSecret)
