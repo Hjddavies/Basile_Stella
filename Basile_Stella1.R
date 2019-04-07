a86c9113e7cade2179f4fe1ade74e8daf9cc53f7
Hjddavies <hywel.davies.2@city.ac.uk>
2019-04-07 20:05:13 +0100

    Add files via upload

---
+++ Basile_Stella1.R
@@ -0,0 +1,13 @@
+Initial ideas from https://www.interhacktives.com/2017/01/25/scrape-tweets-r-journalists/ by Ryan Watts
+
+library(stringr)
+library(twitteR)
+library(purrr)
+library(tidytext)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(scales)
+library(broom)
+library(ggplot2)
+library(tidyverse)
